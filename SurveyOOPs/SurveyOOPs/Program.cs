7a45a969531a3403a07209dae64a6ff4ff0b95e0
Harika <harikaballa163@gmail.com>
2021-05-28 11:35:19 +0530

    Added Survey file Signed-off-by: Harika <harikaballa163@gmail.com>

---
+++ SurveyOOPs/SurveyOOPs/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SurveyOOPs
+{
+    class student
+    {
+        public String name;
+        public int age;
+        public string month;
+        public int grade;
+        public void display()
+        {
+            Console.WriteLine("name:{0} ,age:{1},month:{2},grade:{3}",name,age,month,grade);
+
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var student = new student();
+            Console.WriteLine("student name");
+            student.name = Console.ReadLine();
+
+            Console.WriteLine("student age");
+            student.age= int.Parse(Console.ReadLine());
+
+            Console.WriteLine("birth month");
+             student.month = Console.ReadLine();
+
+            Console.WriteLine("grade");
+            student.grade=int.Parse(Console.ReadLine());
+
+            student.display();
+
+            
+        }
+    }
+    
+}
