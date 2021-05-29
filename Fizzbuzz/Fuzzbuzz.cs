02bb90906f48b6154c597742d5749cc72d3d621e
Harika <harikaballa163@gmail.com>
2021-05-29 15:59:26 +0530

    branch development

---
+++ Fizzbuzz/Fuzzbuzz.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace sample
+{
+    class Fuzzbuzz
+    {
+      
+            static void fizzBuzz()
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    if (i % 3 == 0 && i%5==0)
+                    {
+                        Console.WriteLine("FizzBuzz");
+                    }
+                    else if (i % 5 == 0)
+                    {
+                        Console.WriteLine("Buzz");
+                    }
+                    else if (i % 3 == 0)
+                    {
+                        Console.WriteLine("Fizz");
+                    }
+                    else
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+            static void Main(string[] args)
+            {
+                fizzBuzz();
+            }
+           
+        
+    }
+}
