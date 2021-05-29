2dcec293fb11f4f3f496957a092c383eec2f08d1
Harika <harikaballa163@gmail.com>
2021-05-29 16:26:41 +0530

    FindMin feature branch

---
+++ FindMin/Minfind.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FindMin
+{
+    class Minfind
+    {
+        static int findMin(int[] b)
+        {
+            int min = b[1];
+            for(int i=0;i<b.Length;i++)
+            {
+                int temp = b[i];
+               int nextelement = b[i];
+                if(min>temp)
+                {
+                    min =b[i];
+                }
+            }
+            return min;
+        }
+        static void Main(string[] args)
+        {
+            int[] a = { 78, 37, 26, 49 ,2};
+            int minvalue = findMin(a);
+
+            Console.WriteLine(minvalue);
+        }
+    }
+}
